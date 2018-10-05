876db3cda1667b8275ef82c1851cbf24a183c11b
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2018-10-05 22:10:29 +0200

    feat: add autopause and customization of the shape

--- index.js
+++ index.js
@@ -5,10 +5,15 @@
 global.BMP = 120;
 global.SamplingFrequency = 44100;
 global.N = 60 / global.BMP * 4; // Czas trwania nuty w sekundach
+global.N2 = global.N * 2; // Czas trwania dwóch nut
+global.N3 = global.N * 3; // Czas trwania trzech nut
+global.N4 = global.N * 5; // Czas trwania pieciu nut
 global.H = global.N / 2; // Czas trwania półnuty w sekundach
 global.Q = global.N / 4; // Czas trwania ćwierćnuty w sekundach
 global.E = global.N / 8; // Czas trwania ósemki w sekundach
 global.S = global.N / 16; // Czas trwania szesnastki w sekundach
+global.T = global.N / 32; // Czas trwania trzydziestkidwójki w sekundach
+global.HT = global.N / 64; // Czas trwania sześćdziesięcioczwórki w sekundach
 /*
  * Założono częstotliwość próbkowania równą 44100 Hz, co oznacza, że każda sekunda odpowiada tablicy 
  * N bitowych integer-ów oraz 8-bitowy rozmiar, który wyznacza głośność dzwięku.
@@ -56,4 +61,67 @@ melody2.enqueueTone(global.E, note('E4'));
 melody2.enqueueTone(global.E, note('F4'));
 melody2.enqueueTone(global.H, note('D4'));
 WaveGenerator.save('./starwars.wav', melody2);
+
+let melody3 = new MelodyQueue(global.HT);
+melody3.setAutoPause(global.HT);
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'), note('G4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+melody3.enqueueTone(global.E, note('C4'), note('E4'));
+for (let i = 0; i < 14; ++i) {
+	melody3.enqueueTone(i == 7 ? global.H : global.E, [note('B3'), note('D4'), note('E4')]);
+}
+melody3.setAutoPause(0);
+melody3.enqueuePause(global.T);
+melody3.enqueueTone(global.Q, note('E4'));
+melody3.enqueueTone(global.Q, note('G4'));
+melody3.enqueueTone(global.H, note('A4'));
+melody3.enqueuePause(global.HT);
+melody3.enqueueTone(global.Q, note('A4'));
+melody3.enqueueTone(global.Q, note('B4'));
+melody3.enqueueTone(global.H, note('E4'));
+melody3.enqueuePause(global.HT);
+melody3.enqueueTone(global.Q, note('E4'));
+melody3.enqueueTone(global.Q, note('G4'));
+melody3.enqueueTone(global.H, note('A4'));
+melody3.enqueuePause(global.HT);
+melody3.enqueueTone(global.Q, note('A4'));
+melody3.enqueueTone(global.Q, note('B4'));
+melody3.enqueueTone(global.H, note('E4'));
+melody3.enqueuePause(global.HT);
+melody3.enqueueTone(global.E, note('D4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('D4'));
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, note('D4'));
+melody3.enqueueTone(global.H, note('E4'));
+melody3.setAutoPause(global.HT);
+melody3.enqueueTone(global.E, note('C4'));
+melody3.enqueueTone(global.E, [note('B3'), note('C4')]);
+melody3.enqueueTone(global.E, [note('B3'), note('C4')]);
+melody3.enqueueTone(global.E, [note('A3'), note('C4')]);
+melody3.enqueueTone(global.E, [note('A3'), note('C4')]);
+melody3.enqueueTone(global.E, [note('G3'), note('C4')]);
+melody3.enqueueTone(global.E, [note('C4'), note('F4')]);
+melody3.enqueueTone(global.H, [note('B3'), note('E4')]);
+WaveGenerator.save('./rocky.wav', melody3);
+
 */
--- src/MelodyQueue.js
+++ src/MelodyQueue.js
@@ -2,19 +2,30 @@ const tone = require('tonegenerator');
 
 class MelodyQueue
 {
-    constructor() {
+    constructor(autopause = 0, shape = 'sine') {
+		this.autopause = autopause;
+		this.shape = shape;
         this.queue = [];
     }
+	setAutoPause(duration) {
+		this.autopause = duration;
+	}
     enqueueTone(duration, ...frequencies) {
         let tones = [];
+		if (Array.isArray(frequencies[0])) {
+			frequencies = frequencies[0];
+		}
         for (let frequency of frequencies) {
             tones.push(tone({
 				freq: frequency,
 				lengthInSecs: duration,
 				volume: 30,
 				rate: global.SamplingFrequency,
-				shape: 'sine'
+				shape: this.shape
 			}));
+        }
+		for (let i = 0; i < global.SamplingFrequency * this.autopause; ++i) {
+            this.queue.push(0);
         }
         for (let i = 0; i < tones[0].length; ++i) {
             let sound = tones[0][i];
@@ -29,6 +40,21 @@ class MelodyQueue
             this.queue.push(0);
         }
     }
+	mix(melodyQueue) {
+		let newMelodyQueue = new MelodyQueue();
+		const limit = melodyQueue.queue.length > this.queue.length ? melodyQueue.queue.length : this.queue.length;
+		for (let i = 0; i < limit; ++i) {
+			let sound = 0;
+			if (i < melodyQueue.queue.length) {
+				sound += melodyQueue.queue[i];
+			}
+			if (i < this.queue.length) {
+				sound += melodyQueue.queue[i];
+			}
+			newMelodyQueue.push(sound);
+		}
+		return newMelodyQueue;
+	}
 }
 
 module.exports = MelodyQueue;
