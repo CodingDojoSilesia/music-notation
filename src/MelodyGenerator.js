ac5a25c863d0a24a85f31484ce3e70221e4c6b65
Firemark <marpiechula@gmail.com>
2018-10-10 01:25:38 +0200

    add tests frameworks with example tests

--- src/MelodyGenerator.js
+++ src/MelodyGenerator.js
@@ -1,7 +1,18 @@
+require('./globals.js');
+const MelodyQueue = require('./MelodyQueue.js');
+const waveGenerator = require('./WaveGenerator.js');
+const note = require('./note.js');
+
 class MelodyGenerator
 {
     fromString(melodyString) {
         // put your code here
+        let melody = new MelodyQueue();
+        melody.enqueueTone(global.Q, [note('A4')]);
+    }
+
+    getNotesArray(notes) {
+        return notes.map((n) => note(n));
     }
 }
 
