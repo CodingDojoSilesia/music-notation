5ae90ec924ffa496e86132b891f259776abde846
Firemark <marpiechula@gmail.com>
2018-10-11 00:00:39 +0200

    rewrite tests to jest framework

--- test/basic.test.js
+++ test/basic.test.js
@@ -4,38 +4,31 @@ const waveGenerator = require('../src/WaveGenerator.js');
 const MelodyGenerator = require('../src/MelodyGenerator.js');
 const note = require('../src/note.js');
 
-const assert = require('chai').assert;
-// https://www.chaijs.com/api/assert/
-const sinon = require('sinon');
-// https://sinonjs.org/releases/v6.3.4/spies/
-// https://sinonjs.org/
+// https://jestjs.io/docs/en/using-matchers
+// https://jestjs.io/docs/en/es6-class-mocks
 
-describe('MelodyGenerator.getNotesArray', () => {
-    it('getNotesArray(["A4"]) should returns note("A4")', () => {
-        let notes = MelodyGenerator.getNotesArray(['A4']);
-        assert.deepEqual(notes, [note('A4')]);
-    });
+jest.mock('../src/MelodyQueue.js');
 
-    it('getNotesArray(["A4", "B4"]) should returns A4 and B4 notes', () => {
-        let notes = MelodyGenerator.getNotesArray(['A4', 'B4']);
-        assert.deepEqual(notes, [note('A4'), note('B4')]);
-    });
+beforeEach(() => {
+    MelodyQueue.mockClear();
 });
 
-describe('MelodyGenerator.fromString', () => {
-    let enqueueTone = null;
-    beforeEach(() => {
-        enqueueTone = sinon.spy(MelodyQueue.prototype, 'enqueueTone');
-    });
-
-    after(() => {
-        MelodyQueue.prototype.enqueueTone.restore();
-    })
-
-    it('should plays note(A4, Q)', () => {
-        MelodyGenerator.fromString('note(A4, Q)');
-        assert(
-            enqueueTone.calledWith(global.Q, [note('A4')])
-        );
-    });
+test('getNotesArray(["A4"]) should returns note("A4")', () => {
+    let notes = MelodyGenerator.getNotesArray(['A4']);
+    expect(notes).toEqual([note('A4')]);
 });
+
+test('getNotesArray(["A4", "B4"]) should returns A4 and B4 notes', () => {
+    let notes = MelodyGenerator.getNotesArray(['A4', 'B4']);
+    expect(notes).toEqual([note('A4'), note('B4')]);
+});
+
+test('MelodyGenerator.fromString("note(A4, Q)") should plays note(A4, Q)', () => {
+    MelodyGenerator.fromString('note(A4, Q)');
+    let melodyQueue = MelodyQueue.mock.instances[0];
+    (
+        expect(melodyQueue.enqueueTone)
+        .toHaveBeenCalledWith(global.Q, [note('A4')])
+    );
+});
+
