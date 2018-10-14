49b5d94b792820626f3182cfd3bb2d1a7934dff8
Firemark <marpiechula@gmail.com>
2018-10-14 22:24:34 +0200

    write tests + logic

--- src/MelodyGenerator.js
+++ src/MelodyGenerator.js
@@ -14,20 +14,98 @@ class MelodyGenerator {
     }
 
     parse(line) {
-        let match = line.match(/note\(([\w;]+)\s*,\s*(\w)\)/);
+        line = line.trim();
+        return (
+            this.parseDefine(line)
+            || this.parseMusic(line)
+            || null
+        );
+    }
+
+    parseDefine(line) {
+        const match = line.match(/^define\s*(\w+)\s*is\s*(.+)$/);
         if (!match) {
-            return;
+            return null;
+        }
+        return {
+            type: 'define',
+            name: match[1],
+            elements: this._parseElements(match[2])
+        }
+    }
+
+    parseMusic(line) {
+        const match = line.match(/^play\s*(.+)$/);
+        if (!match) {
+            return null;
+        }
+        return {
+            type: 'play',
+            elements: this._parseElements(match[1])
+        }
+    }
+
+    _parseElements(line) {
+        const notes = line.split('|');
+        return notes.map(this.parseSingleNote.bind(this));
+    }
+
+    parseSingleNote(strNote) {
+        strNote = strNote.trim();
+        return (
+            this.matchRepeat(strNote)
+            || this.matchNote(strNote)
+            || this.matchPause(strNote)
+            || this.matchFunc(strNote)
+            || null
+        );
+    }
+
+    matchRepeat(strNote) {
+        const match = strNote.match(/^repeat\s*(\d+)\s*times\s*(.+)$/);
+        if (!match) {
+            return null;
+        }
+        return {
+            type: 'repeat',
+            times: parseInt(match[1]) || 0,
+            body: this.parseSingleNote(match[2])
+        };
+    }
+
+    matchNote(strNote) {
+        const match = strNote.match(/^note\(([\w;]+)\s*,\s*(\w)\)$/);
+        if (!match) {
+            return null;
+        }
+        return {
+            type: 'note',
+            notes: match[1].split(/\s*;\s*/),
+            time: match[2]
+        };
+    }
+
+    matchPause(strNote) {
+        const match = strNote.match(/^pause\((\w+)\)$/);
+        if (!match) {
+            return null;
         }
 
         return {
-            type: 'music',
-            elements: [
-                {
-                    type: 'note',
-                    notes: match[1].split(/\s*;\s*/),
-                    time: match[2]
-                }
-            ]
+            type: 'pause',
+            time: match[1],
+        }
+    }
+
+    matchFunc(strNote) {
+        const match = strNote.match(/^[a-zA-Z]+$/);
+        if (!match) {
+            return null;
+        }
+
+        return {
+            type: 'function',
+            name: match[0]
         };
     }
 }
--- test/basic.test.js
+++ test/basic.test.js
@@ -14,27 +14,27 @@ beforeEach(() => {
 });
 
 test('getNotesArray(["A4"]) should returns note("A4")', () => {
-    let notes = MelodyGenerator.getNotesArray(['A4']);
+    const notes = MelodyGenerator.getNotesArray(['A4']);
     expect(notes).toEqual([note('A4')]);
 });
 
 test('getNotesArray(["A4", "B4"]) should returns A4 and B4 notes', () => {
-    let notes = MelodyGenerator.getNotesArray(['A4', 'B4']);
+    const notes = MelodyGenerator.getNotesArray(['A4', 'B4']);
     expect(notes).toEqual([note('A4'), note('B4')]);
 });
 /*
     MelodyGenerator.fromString('note(A4, Q)');
-    let melodyQueue = MelodyQueue.mock.instances[0];
+    const melodyQueue = MelodyQueue.mock.instances[0];
     (
         expect(melodyQueue.enqueueTone)
         .toHaveBeenCalledWith(global.Q, [note('A4')])
     );
 */
 
-test('parse("note(A4, Q)")', () => {
-    let data = MelodyGenerator.parse('note(A4, Q)');
+test('parse("play note(A4, Q)")', () => {
+    const data = MelodyGenerator.parse('play note(A4, Q)');
     expect(data).toEqual({
-        type: 'music',
+        type: 'play',
         elements: [
             {type: 'note', notes: ['A4'], time: 'Q'}
         ]
@@ -42,11 +42,81 @@ test('parse("note(A4, Q)")', () => {
 });
 
 test('parse("note(A4;B4;C4, Q)")', () => {
-    let data = MelodyGenerator.parse('note(A4;B4;C4, Q)');
+    const data = MelodyGenerator.parse('play note(A4;B4;C4, Q)');
     expect(data).toEqual({
-        type: 'music',
+        type: 'play',
         elements: [
             {type: 'note', notes: ['A4','B4','C4'], time: 'Q'}
         ]
     });
 });
+
+test('parse("note(A4, Q) | note(B4, H)")', () => {
+    const data = MelodyGenerator.parse('play note(A4, Q) | note(B4, H)');
+    expect(data).toEqual({
+        type: 'play',
+        elements: [
+            {type: 'note', notes: ['A4'], time: 'Q'},
+            {type: 'note', notes: ['B4'], time: 'H'}
+        ]
+    });
+});
+
+test('parse("foo")', () => {
+    const data = MelodyGenerator.parse('play foo');
+    expect(data).toEqual({
+        type: 'play',
+        elements: [
+            {type: 'function', name: 'foo'}
+        ]
+    });
+});
+
+test('parse("note(A4, Q) | foo")', () => {
+    const data = MelodyGenerator.parse('play note(A4, Q) | foo');
+    expect(data).toEqual({
+        type: 'play',
+        elements: [
+            {type: 'note', notes: ['A4'], time: 'Q'},
+            {type: 'function', name: 'foo'}
+        ]
+    });
+});
+
+test('parse("pause(Q) | note(A4, H)")', () => {
+    const data = MelodyGenerator.parse('play pause(Q) | note(A4, H)')
+    expect(data).toEqual({
+        type: 'play',
+        elements: [
+            {type: 'pause', time: 'Q'},
+            {type: 'note', notes: ['A4'], time: 'H'}
+        ]
+    })
+});
+
+test('parse("repeat 3 times note(C4, Q) | foobar")', () => {
+    const data = MelodyGenerator.parse('play repeat 3 times note(C4, Q) | foobar')
+    expect(data).toEqual({
+        type: 'play',
+        elements: [
+            {
+                type: 'repeat',
+                times: 3,
+                body: {type: 'note', notes: ['C4'], time: 'Q'}
+            },
+            {type: 'function', name: 'foobar'}
+        ]
+    });
+});
+
+test('parse("define socek is note(A4, Q) | foobar")', () => {
+    const data = MelodyGenerator.parse('define socek is note(A4, Q) | foobar')
+    expect(data).toEqual({
+        type: 'define',
+        name: 'socek',
+        elements: [
+            {type: 'note', notes: ['A4'], time: 'Q'},
+            {type: 'function', name: 'foobar'}
+        ]
+    });
+})
