3c24e4b171609e4cef348ee33745f91453561d8e
Marek Piechula <m.piechula@clearcode.cc>
2018-10-17 22:00:44 +0200

    add transform method

--- src/MelodyQueue.js
+++ src/MelodyQueue.js
@@ -35,14 +35,30 @@ class MelodyQueue
     }
 
     combiningTonesAndPushToQueue(tones, sampleLimit) {
-        for (let i = 0; i < sampleLimit; ++i) {
-            let sound = tones[0][i];
-            for (let j = 1; j < tones.length; ++j) {
-                let newSound = tones[j][i];
+        for (let sample = 0; sample < sampleLimit; ++sample) {
+            let sound = tones[0][sample];
+            for (let toneIndex = 1; toneIndex < tones.length; ++toneIndex) {
+                let newSound = tones[toneIndex][sample];
                 sound = 2 * (sound + newSound) - sound * newSound / SILENCE - SILENCE * 2;
             }
+            sound = this.transformSound(sound, sample, sampleLimit)
             this.queue.push(sound);
         }
+    }
+
+    transformSound(sound, sample, sampleLimit) {
+        let withoutOffsetSound = sound - SILENCE;
+        let time = sample / sampleLimit;
+        let amplitude = this.getAmplitudeToTransform(time);
+
+        return (withoutOffsetSound * amplitude) + SILENCE;
+    }
+
+    getAmplitudeToTransform(time, stop) {
+        if (time < 0.05) { return time / 0.05; }
+        if (time < 0.5) { return time / -0.8725 + 1.071; }
+        if (time < 1.0) { return time / -2.0 + 0.75; }
+        return 0;
     }
 
     getLimit(tones) {
