08c410aba6c97df2575199867bcf3e953f80c8c3
kamilborecki <k.borecki@clearcode.cc>
2018-11-30 19:13:43 +0100

    code clean up

--- src/MelodyGenerator.js
+++ src/MelodyGenerator.js
@@ -1,6 +1,5 @@
 const durations = require('./durations.js');
 const MelodyQueue = require('./MelodyQueue.js');
-const waveGenerator = require('./WaveGenerator.js');
 const StringParser = require('./StringParser');
 const note = require('./note.js');
 
@@ -8,7 +7,7 @@ class MelodyGenerator {
   fromString(melodyString) {
     const serializer = new StringParser(melodyString);
     const steps = serializer.getSteps();
-    let melody = new MelodyQueue();
+    const melody = new MelodyQueue();
     steps.forEach(step => {
       if (step.notes) {
         melody.enqueueTone(durations[step.duration], step.notes.length > 1 ? this.getNotesArray(step.notes) : note(step.notes[0]));
